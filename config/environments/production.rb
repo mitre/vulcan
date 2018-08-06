4826a0653ba5a354ef191520a4f886100b72894c
Luke Malinowski <lmalinowski@mitre.org>
2018-08-06 09:21:50 -0400

    Patch out asset and secrets hacks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.unknown_asset_fallback = true
+  config.assets.unknown_asset_fallback = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
