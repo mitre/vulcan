8132724c85c2a7cb7cb10d8aa3076c460cb89add
Luke Malinowski <lmalinowski@mitre.org>
2018-08-21 13:16:54 -0400

    Add Support for baseurl hosting through puma and re-add spring for dev

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ COPY Gemfile.lock Gemfile.lock
 RUN gem install bundler && bundle install --jobs 20 --retry 5
 
 ENV RAILS_ENV=production
-#ENV RAILS_RELATIVE_URL_ROOT=/vulcan
+ENV RAILS_RELATIVE_URL_ROOT=/vulcan
 ENV RAILS_SERVE_STATIC_FILES=true
 
 COPY . .
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if ENV['RAILS_ENV'] != "production"
+  Spring.watch "app/services/**"
+end
+
 module VulcanNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
