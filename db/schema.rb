93b91e568a2f50851a6366c790f2896904c75d3b
Tollick <mtollick@mm212956-pc.mitre.org>
2018-11-05 17:48:43 -0500

    Fixed login, implemented Heimdall login function
    Signed-off-by: Tollick <mtollick@mm212956-pc.mitre.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180810164306) do
+ActiveRecord::Schema.define(version: 20181101174651) do
 
   create_table "ccis", force: :cascade do |t|
     t.string "cci"
@@ -29,6 +29,30 @@ ActiveRecord::Schema.define(version: 20180810164306) do
     t.string "status"
     t.integer "project_control_history_id"
     t.index ["project_control_history_id"], name: "index_control_change_status_on_project_control_history_id"
+  end
+
+  create_table "db_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_db_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_db_users_on_reset_password_token", unique: true
   end
 
   create_table "host_configs", force: :cascade do |t|
@@ -52,6 +76,23 @@ ActiveRecord::Schema.define(version: 20180810164306) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_users_host_configs_on_user_id"
+  end
+
+  create_table "ldap_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_ldap_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_ldap_users_on_reset_password_token", unique: true
   end
 
   create_table "nist_controls", force: :cascade do |t|
