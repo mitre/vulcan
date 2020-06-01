0c76cbd2f64c90c53e67b8d4a00274ffc5418f40
Robert Clark <rbclark@mitre.org>
2020-06-01 17:42:39 -0400

    Use 'let' to setup user for controller testing, verify users sign in count isn't changed

--- spec/features/local_login_spec.rb
+++ spec/features/local_login_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 RSpec.describe 'Local Login', type: :feature do
   include LoginHelpers
 
-  context 'when user login is incorrect' do
-    let(:user) { create(:user) }
+  let(:user1) { create(:user) }
 
+  context 'when user login is incorrect' do
     it 'shows error banner when login credentials are incorrect' do
-      credentials = { 'user_email' => user.email, 'user_password' => 'bad_pass' }
+      credentials = { 'user_email' => user1.email, 'user_password' => 'bad_pass' }
       expect { vulcan_sign_in_with('Local Login', credentials) }
-        .not_to change(User, :count)
+        .not_to change(user1, :sign_in_count)
 
       expect(page)
         .to have_selector('.alert-danger', text: 'Invalid Email or password.')
