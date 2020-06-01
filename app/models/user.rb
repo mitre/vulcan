7c6e79f451e9c5488999e4130fe6dc0a5f2a187a
John <atwoodj@mitre.org>
2020-06-01 13:52:27 -0400

    Allow users to optionally enable or disable email confirmation.
    This setting can be configured by setting it in vulcan.yml.
    
    Signed-off-by: John <atwoodj@mitre.org>

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :name, :password, presence: true
 
+  before_create :skip_confirmation!, unless: -> { Settings.local_login.email_confirmation }
+
   def self.from_omniauth(auth)
     find_or_create_by(email: auth.info.email) do |user|
       user.email = auth.info.email
