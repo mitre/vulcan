b9856998c5c83dc117247131c02d8cf45832a67d
John <atwoodj@mitre.org>
2020-06-02 11:19:36 -0400

    added configurable email
    Signed-off-by: John <atwoodj@mitre.org>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,6 +3,6 @@
 # This is the base mailer for the application. Things should only be
 # placed here if they are shared between multiple mailers
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: Settings.contact_email
   layout 'mailer'
 end
