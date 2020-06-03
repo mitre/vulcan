7317a8996c92948268c7c1618371edca5b97b91b
John Atwood <jsa5593@rit.edu>
2020-06-03 13:45:18 -0400

    Email config (#101)
    * fixed duplicate error on registration
    
    * added configurable email
    
    * added tests for contact email address
    
    Authored-by: John <atwoodj@mitre.org>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,16 +8,8 @@ Devise.setup do |config|
   # config.parent_controller = 'DeviseController'
 
   # ==> Mailer Configuration
-  # Configure the e-mail address which will be shown in Devise::Mailer,
-  # note that it will be overwritten if you use your own mailer class
-  # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
-  # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
-
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
