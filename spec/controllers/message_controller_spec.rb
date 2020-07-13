1420538613ac7ec1f616b940ac568e9fd6c2361f
John <atwoodj@mitre.org>
2020-07-13 09:11:07 -0400

    updating tests
    Signed-off-by: John <atwoodj@mitre.org>

---
+++ spec/controllers/message_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe MessagesController  do
+    context 'Creates message' do
+        # it 'posts message to channel' do
+        #     expect { post :create, message: { body: 'test' } }.to
+        #     have_broadcasted_to("notifications_channel").with(body: 'test')
+        # end
+    end
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe PagesController  do
+    context 'page controll' do
+        it 'message pass' do
+        end
+    end
+end
