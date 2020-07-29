a759924f733d1dffd641171a9bf1732e6399a086
John <atwoodj@mitre.org>
2020-07-29 12:45:26 -0400

    updated bell and fixed page reload bug
    Signed-off-by: John <atwoodj@mitre.org>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@
 # This allows for the home page to have access to all of the messages
 class PagesController < ApplicationController
   def home
-    current_user&.update_messages_stamp
     @messages = Message.all
     @message = Message.new
   end
