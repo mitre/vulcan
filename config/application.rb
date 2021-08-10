e59fb40b5f1772967bb50aa45fe7d8124ac52e47
Kyle Fagan <kfagan@mitre.org>
2021-08-10 13:39:47 -0700

    Set application time zone

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module VulcanVue
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'UTC'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
