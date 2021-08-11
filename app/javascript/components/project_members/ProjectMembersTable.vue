3bc7406b72f87d728c08e40c8590d8b547d6591a
Justin Kufro <jkufro@mitre.org>
2021-08-11 13:14:02 -0600

    add search to project members table component

--- app/javascript/components/project_members/ProjectMembersTable.vue
+++ app/javascript/components/project_members/ProjectMembersTable.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h2>Project Members</h2>
+
+    <!-- User search -->
+    <div class="row">
+      <div class="col-6">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <div class="input-group-text"><i class="mdi mdi-magnify" aria-hidden="true"></i></div>
+          </div>
+          <input type="text" class="form-control" id="userSearch" placeholder="Search users by name or email..." v-model="search">
+        </div>
+      </div>
+    </div>
+
+    <br/>
     
     <table class="table">
       <tr>
@@ -12,7 +26,7 @@
                      v-bind:project_member="project_member"
                      v-bind:project="project"
                      v-bind:available_roles="available_roles"
-                     v-for="project_member in project_members" />
+                     v-for="project_member in searchedProjectMembers" />
     </table>
   </div>
 </template>
@@ -32,6 +46,17 @@ export default {
     available_roles: {
       type: Array,
       required: true,
+    }
+  },
+  data: function () {
+    return {
+      search: ""
+    }
+  },
+  computed: {
+    searchedProjectMembers: function () {
+      let downcaseSearch = this.search.toLowerCase()
+      return this.project_members.filter(pm => pm.email.toLowerCase().includes(downcaseSearch) || pm.name.toLowerCase().includes(downcaseSearch));
     }
   }
 }
