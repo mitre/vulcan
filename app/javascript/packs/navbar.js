a99cd252bc0b1623ee5b7e8c0844c533e5bd028d
Kyle Fagan <kfagan@mitre.org>
2021-08-11 15:45:29 -0700

    Remove unused bootstrap-vue button import

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,12 +1,11 @@
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue'
-import {NavbarPlugin, ButtonPlugin} from 'bootstrap-vue'
+import {NavbarPlugin} from 'bootstrap-vue'
 import Navbar from '../components/navbar/App.vue'
 import NavbarItem from '../components/navbar/NavbarItem.vue'
 
 Vue.use(TurbolinksAdapter)
 Vue.use(NavbarPlugin)
-Vue.use(ButtonPlugin)
 
 Vue.component('navbar', Navbar)
 Vue.component('NavbarItem', NavbarItem)
