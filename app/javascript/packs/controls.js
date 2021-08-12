e01c7567656ae4da980af05c413c237a46a0b0a3
Justin Kufro <jkufro@mitre.org>
2021-08-12 11:38:02 -0600

    ControlComment.vue added with placeholder from submit

--- app/javascript/packs/controls.js
+++ app/javascript/packs/controls.js
@@ -1,6 +1,6 @@
 import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue'
-import {ButtonPlugin, BreadcrumbPlugin} from 'bootstrap-vue'
+import {ButtonPlugin, BreadcrumbPlugin, FormPlugin, FormGroupPlugin, FormTextareaPlugin} from 'bootstrap-vue'
 import Controls from '../components/controls/Controls.vue'
 import ControlsCodeEditorView from '../components/controls/ControlsCodeEditorView.vue'
 import ControlComments from '../components/controls/ControlComments.vue'
@@ -9,6 +9,9 @@ import ControlNavigator from '../components/controls/ControlNavigator.vue'
 Vue.use(TurbolinksAdapter)
 Vue.use(ButtonPlugin)
 Vue.use(BreadcrumbPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormGroupPlugin)
+Vue.use(FormTextareaPlugin)
 
 Vue.component('Controls', Controls)
 Vue.component('ControlsCodeEditorView', ControlsCodeEditorView)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,13 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :rule
+
+  delegate :name, to: :user
+
+  ##
+  # Override `as_json` to include delegated attributes
+  #
+  def as_json(options = {})
+    super options.merge(methods: %i[name])
+  end
 end
