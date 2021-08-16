c91d4092e7456942825739b38e3dac5c1ea33a11
Kyle <Bialogs@users.noreply.github.com>
2021-08-16 15:39:53 -0700

    audited config for project (#199)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@
 
 # Projects are home to a collection of Rules and are managed by Users.
 class Project < ApplicationRecord
+  audited except: %i[id created_at updated_at project_members_count], max_audits: 1000
+
   has_many :project_members, dependent: :destroy
   has_many :users, through: :project_members
   has_many :rules, dependent: :destroy
