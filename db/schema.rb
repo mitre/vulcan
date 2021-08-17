3b05691227d49913bbff079da567f5df6afc8055
Kyle Fagan <kfagan@mitre.org>
2021-08-17 11:53:42 -0700

    Add associated arbitrary metadata to Projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_211538) do
+ActiveRecord::Schema.define(version: 2021_08_17_181615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,14 @@ ActiveRecord::Schema.define(version: 2021_08_16_211538) do
     t.index ["project_id"], name: "index_project_members_on_project_id"
     t.index ["user_id", "project_id"], name: "by_user_and_project", unique: true
     t.index ["user_id"], name: "index_project_members_on_user_id"
+  end
+
+  create_table "project_metadata", force: :cascade do |t|
+    t.json "data", null: false
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "by_project_id", unique: true
   end
 
   create_table "projects", force: :cascade do |t|
