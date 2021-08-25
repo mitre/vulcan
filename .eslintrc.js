b5911e6c8c0d50ecace80212913ae0bfbc5d05ba
Robert Clark <rbclark@mitre.org>
2021-08-25 17:09:29 -0400

    Add eslint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:vue/recommended", 'plugin:prettier/recommended', 'prettier' ],
+  rules: {
+    'no-console': 'warn',
+    'no-return-await': 'warn',
+    'no-throw-literal': 'warn',
+    'vue/require-default-prop': 'off',
+    'vue/prop-name-casing': 'off',
+    'object-curly-spacing': 'off',
+    'lines-between-class-members': [
+      'warn',
+      'always',
+      {exceptAfterSingleLine: true}
+    ],
+    'padding-line-between-statements': [
+      'warn',
+      {blankLine: 'always', prev: 'function', next: '*'},
+      {blankLine: 'always', prev: 'import', next: ['class', 'function']}
+    ],
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ]
+  }
+};
