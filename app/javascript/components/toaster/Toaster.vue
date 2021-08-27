5d966dd1aacf180f425d8dea74fe3b02201a9d67
Justin Kufro <jkufro@mitre.org>
2021-08-27 10:53:47 -0600

    page load toasts added in replacement of alerts

---
+++ app/javascript/components/toaster/Toaster.vue
@@ -0,0 +1,32 @@
+<template>
+  <div />
+</template>
+
+<script>
+import AlertMixinVue from "../../mixins/AlertMixin.vue";
+
+export default {
+  name: "Toaster",
+  mixins: [AlertMixinVue],
+  props: {
+    notice: {
+      type: String,
+      required: false,
+    },
+    alert: {
+      type: String,
+      required: false,
+    },
+  },
+  mounted: function () {
+    this.alertOrNotifyResponse({
+      data: {
+        notice: this.notice,
+        alert: this.alert,
+      },
+    });
+  },
+};
+</script>
+
+<style scoped></style>
