3ccfb1288f5ab62cc3f16013781edc139e9c7c6f
Justin Kufro <kufroj@gmail.com>
2021-09-23 11:40:00 -0600

    change SecurityRequirementsGuide.from_mapping to rescue from parsing errors so that user gets usable feedback in the UI (#237)
    Co-authored-by: Justin Kufro <jkufro@mitre.org>

--- app/models/security_requirements_guide.rb
+++ app/models/security_requirements_guide.rb
@@ -11,12 +11,16 @@ class SecurityRequirementsGuide < ApplicationRecord
 
   # Since an SRG is top-level, the parameter is the entire parsed benchmark
   def self.from_mapping(benchmark_mapping)
-    SecurityRequirementsGuide.new(
-      srg_id: benchmark_mapping.id,
-      title: benchmark_mapping.title.first,
-      version: "V#{benchmark_mapping.version.version}" \
-               "#{SecurityRequirementsGuide.revision(benchmark_mapping.plaintext.first)}"
-    )
+    # Disabling `Style/RescueModifier` here because the goal is simply just to try and
+    # fetch the attribute, but return `nil` if anything goes wrong with parsing.
+    # rubocop:disable Style/RescueModifier
+    id = benchmark_mapping.id rescue nil
+    title = benchmark_mapping.title.first rescue nil
+    version = "V#{benchmark_mapping.version.version}" \
+              "#{SecurityRequirementsGuide.revision(benchmark_mapping.plaintext.first)}" rescue nil
+    # rubocop:enable Style/RescueModifier
+
+    SecurityRequirementsGuide.new(srg_id: id, title: title, version: version)
   end
 
   # If the SRGs do not conform nicely and this function gets complex, remove the version parse logic
