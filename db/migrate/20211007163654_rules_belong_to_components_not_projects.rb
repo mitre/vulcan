ec19aea269f6aa4033b76358989e1a73c8742d7c
Justin Kufro <jkufro@mitre.org>
2021-10-07 10:48:38 -0600

    revise DB schema based on new planned schema

---
+++ db/migrate/20211007163654_rules_belong_to_components_not_projects.rb
@@ -0,0 +1,9 @@
+class RulesBelongToComponentsNotProjects < ActiveRecord::Migration[6.1]
+  def change
+    # Rules should no longer belong to projects
+    remove_reference :rules, :project, foreign_key: true
+
+    # Rules should instead now only belong to components
+    add_reference :rules, :component, foreign_key: true
+  end
+end
