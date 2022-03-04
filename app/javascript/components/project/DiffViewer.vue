2f2a099852226fba0864311cedc68c01a1061f56
Wong, Tim <twong@mitre.org>
2022-03-04 11:38:47 -0500

    more diff viewer progress

--- app/javascript/components/project/DiffViewer.vue
+++ app/javascript/components/project/DiffViewer.vue
@@ -2,18 +2,37 @@
   <div class="my-1">
     <b-row class="my-1">
       <b-col md="2">
-        <div
-          v-for="rule_id in Object.keys(diffRules)"
-          :key="`rule-${rule_id}`"
-          :class="ruleRowClass(rule_id)"
-          @click="ruleSelected(rule_id)"
-        >
-          {{ baseComponent.prefix }}-{{ rule_id }}
-          <i
-            v-if="diffRules[rule_id].changed"
-            class="mdi mdi-file-document-edit-outline float-right diff-icon"
-            aria-hidden="true"
-          />
+        <div v-if="Object.keys(ruleDiffs).length === 0" class="p-2">
+          <h6>Compare Components:</h6>
+        </div>
+        <div v-else id="sidebar-wrapper">
+          <div id="scrolling-sidebar" ref="sidebar" :style="sidebarStyle">
+            <!-- Filter -->
+            <b-form-group label="Filter">
+              <b-form-checkbox
+                id="rcFilterChecked-filter"
+                v-model="filters.rcFilterChecked"
+                size="sm"
+                class="mb-1 unselectable"
+                name="rcFilterChecked-filter"
+              >
+                <strong>({{ ruleDiffFilterCounts.rc }})</strong> Rule Changed
+              </b-form-checkbox>
+            </b-form-group>
+            <div
+              v-for="rule_id in filteredRuleDiffIds"
+              :key="`rule-${rule_id}`"
+              :class="ruleRowClass(rule_id)"
+              @click="ruleSelected(rule_id)"
+            >
+              {{ baseComponent.prefix }}-{{ rule_id }}
+              <i
+                v-if="ruleDiffs[rule_id].changed"
+                class="mdi mdi-file-document-edit-outline float-right diff-icon"
+                aria-hidden="true"
+              />
+            </div>
+          </div>
         </div>
       </b-col>
       <b-col md="10">
@@ -77,7 +96,7 @@
           </b-button>
         </b-input-group>
         <MonacoEditor
-          v-if="Object.keys(diffRules).length > 0"
+          v-if="baseControl || diffControl"
           :key="selectedRuleId"
           :diff-editor="true"
           :original="baseControl"
@@ -111,6 +130,7 @@ export default {
   },
   data: function () {
     return {
+      sidebarOffset: 0, // How far the sidebar is from the top of the screen
       editorKey: 0,
       monacoEditorOptions: {
         automaticLayout: true,
@@ -122,13 +142,69 @@ export default {
       },
       baseComponent: null,
       diffComponent: null,
-      diffRules: {},
+      filters: {
+        rcFilterChecked: true, // Rule Changed
+      },
+      ruleDiffs: {},
+      ruleDiffFilterCounts: {
+        rc: 0,
+      },
+      filteredRuleDiffIds: [],
       selectedRuleId: null,
       baseControl: "",
       diffControl: "",
     };
   },
+  computed: {
+    sidebarStyle: function () {
+      return {
+        "max-height": `calc(100vh - ${this.sidebarOffset}px)`,
+      };
+    },
+  },
+  watch: {
+    ruleDiffs: function () {
+      this.ruleDiffFilterCounts = this.calculateRuleDiffFilterCounts();
+      this.filteredRuleDiffIds = this.filterRuleDiffIds();
+    },
+    filters: {
+      handler() {
+        this.filteredRuleDiffIds = this.filterRuleDiffIds();
+        localStorage.setItem(`diffViewerFilters-${this.componentId}`, JSON.stringify(this.filters));
+      },
+      deep: true,
+    },
+  },
+  mounted: function () {
+    // Persist `filters` across page loads
+    if (localStorage.getItem(`diffViewerFilters-${this.componentId}`)) {
+      try {
+        this.filters = JSON.parse(localStorage.getItem(`diffViewerFilters-${this.componentId}`));
+      } catch (e) {
+        localStorage.removeItem(`diffViewerFilters-${this.componentId}`);
+      }
+    }
+    window.addEventListener("scroll", this.handleScroll);
+    this.handleScroll();
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
   methods: {
+    calculateRuleDiffFilterCounts: function () {
+      return {
+        rc: Object.values(this.ruleDiffs).filter((ruleDiff) => ruleDiff.changed).length,
+      };
+    },
+    filterRuleDiffIds: function (ruleDiffs) {
+      return Object.entries(this.ruleDiffs)
+        .filter(([_, ruleDiff]) => {
+          return this.filters.rcFilterChecked ? ruleDiff.changed : true;
+        })
+        .map(([ruleId, _]) => {
+          return ruleId;
+        });
+    },
     // Dynamically set the class of each rule row
     ruleRowClass: function (rule_id) {
       return {
@@ -139,7 +215,7 @@ export default {
     },
     ruleSelected: function (rule_id) {
       this.selectedRuleId = rule_id;
-      const control = this.diffRules[rule_id];
+      const control = this.ruleDiffs[rule_id];
       this.baseControl = control["base"];
       this.diffControl = control["diff"];
     },
@@ -152,7 +228,7 @@ export default {
         axios
           .get(`/components/${this.baseComponent.id}/compare/${this.diffComponent.id}`)
           .then((response) => {
-            this.diffRules = response.data;
+            this.ruleDiffs = response.data;
           })
           .catch(this.alertOrNotifyResponse);
       }
@@ -161,17 +237,35 @@ export default {
       this.monacoEditorOptions[setting] = value;
       this.editorKey += 1;
     },
+    handleScroll: function () {
+      this.$nextTick(() => {
+        // Get the distance from the top of the sidebar to the top of the page
+        let top = this.$refs.sidebar?.getBoundingClientRect().top;
+        // if top is set and greater than 0 then set the sidebar offset to keep
+        // the scrollbar from going off the page
+        this.sidebarOffset = top > 0 ? top : 0;
+      });
+    },
   },
 };
 </script>
 
 <style scoped>
+#scrolling-sidebar {
+  display: block;
+  overflow-y: auto;
+}
+
 .form-select-sm {
   height: 2rem;
 }
 
 .ruleRow {
   padding: 0.25em;
+}
+
+.ruleRow:hover {
+  background: rgb(0, 0, 0, 0.12);
 }
 
 .selectedRuleRow {
