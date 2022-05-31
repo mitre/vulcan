3c7c61734ea9382db03dcd2a9e91e29a79099c58
Wong, Tim <twong@mitre.org>
2022-05-31 16:18:49 -0400

    user tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  User.destroy_all
+  admin_user = FactoryBot.create(:admin_user)
+  user = FactoryBot.create(:user)
+
+  describe 'updating users' do
+    it 'should update given user when user is admin' do
+      sign_in admin_user
+      put :update, params: { id: user.id, user: { admin: false } }
+      expect(flash[:notice]).to eq 'Successfully updated user.'
+    end
+
+    it 'should not update given when user is not admin' do
+      sign_in user
+      put :update, params: { id: user.id }
+      expect(response).to have_http_status(:found)
+    end
+  end
+
+  describe 'removing users' do
+    it 'when admin' do
+      sign_in admin_user
+      user_1 = FactoryBot.create(:user)
+      delete :destroy, params: { id: user_1.id }
+      expect(flash[:notice]).to eq 'Successfully removed user.'
+    end
+
+    it 'when not admin' do
+      sign_in user
+      user_2 = FactoryBot.create(:user)
+      delete :destroy, params: { id: user_2.id }
+      expect(response).to have_http_status(:found)
+    end
+  end
+end
