1b67a0aee92a4fcb56a3ff22ce3926d9a53898c2
Wong, Tim <twong@mitre.org>
2022-06-06 16:43:56 -0400

    additional projects controller tests

--- spec/controllers/components_controller_spec.rb
+++ spec/controllers/components_controller_spec.rb
@@ -13,12 +13,14 @@ RSpec.describe ComponentsController, type: :controller do
       component = FactoryBot.create(:component)
       sign_in @admin_user
       get :search, params: { q: component.based_on.srg_id }
+
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)['components'].map(&:first)).to eq Component.all.pluck(:id)
     end
 
     it 'should ensure the user is logged in' do
       get :search, params: { q: {} }
+
       expect(response).to have_http_status(:found)
     end
   end
--- spec/controllers/security_requirements_guides_controller_spec.rb
+++ spec/controllers/security_requirements_guides_controller_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe SecurityRequirementsGuidesController, type: :controller do
       [@admin_user, @user].each do |u|
         sign_in u
         get :index, format: :json
+
         expect(response).to have_http_status(:success)
         expect(response.body).to eq SecurityRequirementsGuide.all.order(:srg_id, :version)
                                                              .select(:id, :srg_id, :title, :version, :release_date)
@@ -28,12 +29,14 @@ RSpec.describe SecurityRequirementsGuidesController, type: :controller do
       sign_in @admin_user
       file = fixture_file_upload('./spec/fixtures/files/U_Web_Server_V2R3_Manual-xccdf.xml', 'text/xml')
       post :create, params: { file: file }
+
       expect(response).to have_http_status(:success)
     end
 
     it 'should not create SRG when user is not admin' do
       sign_in @user
       post :create
+
       expect(response).to have_http_status(:found)
     end
   end
@@ -43,12 +46,14 @@ RSpec.describe SecurityRequirementsGuidesController, type: :controller do
       srg = FactoryBot.create(:security_requirements_guide)
       sign_in @admin_user
       delete :destroy, params: { id: srg.id }
+
       expect(flash[:notice]).to eq 'Successfully removed SRG.'
     end
 
     it 'when not admin' do
       sign_in @user
       delete :destroy, params: { id: 1 }
+
       expect(response).to have_http_status(:found)
     end
   end
