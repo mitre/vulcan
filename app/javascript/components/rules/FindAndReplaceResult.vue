ea84cd8fe72c30b524e6c297a59bdca968426ac8
Wong, Tim <twong@mitre.org>
2022-07-07 16:36:18 -0400

    highlight matches

--- app/javascript/components/rules/FindAndReplaceResult.vue
+++ app/javascript/components/rules/FindAndReplaceResult.vue
@@ -1,6 +1,10 @@
 <template>
-  <p v-if="value">
-    <b> {{ attr }} </b> {{ value }}
+  <p>
+    <b>{{ attr }}:</b>
+    <span v-for="(segment, index) in segments" :key="index">
+      <mark v-if="segment.highlighted" class="highlighted-text">{{ segment.text }}</mark>
+      <span v-else>{{ segment.text }}</span>
+    </span>
   </p>
 </template>
 
@@ -9,6 +13,10 @@ import axios from "axios";
 export default {
   name: "FindAndReplaceResult",
   props: {
+    find: {
+      type: String,
+      required: true,
+    },
     attr: {
       type: String,
       required: true,
@@ -19,10 +27,37 @@ export default {
     },
   },
   data: function () {
-    return {};
+    return {
+      segments: [],
+    };
   },
-  methods: {},
+  mounted: function () {
+    const normalizedValue = this.value.toLowerCase();
+    const normalizedFind = this.find.toLowerCase();
+    const matchIndices = [];
+    let currentIndex;
+    let previousIndex = 0;
+    while (true) {
+      currentIndex = normalizedValue.indexOf(normalizedFind, previousIndex);
+      if (currentIndex < 0) {
+        break;
+      }
+      matchIndices.push(currentIndex);
+      previousIndex = currentIndex + 1;
+    }
+    currentIndex = 0;
+    matchIndices.forEach((index) => {
+      this.segments.push({ text: this.value.substring(currentIndex, index), highlighted: false });
+      currentIndex = index + this.find.length;
+      this.segments.push({ text: this.value.substring(index, currentIndex), highlighted: true });
+    });
+    this.segments.push({ text: this.value.substring(currentIndex), highlighted: false });
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.highlighted-text {
+  background-color: #f0dc76;
+}
+</style>
