29c2ad49179a0c09e92453b45767c9003fa667f0
rlakey <rlakey81@outlook.com>
2023-01-18 15:32:35 -0700

    add missing tooltips and updated srg version to id

--- app/javascript/components/rules/RuleSecurityRequirementsGuideInformation.vue
+++ app/javascript/components/rules/RuleSecurityRequirementsGuideInformation.vue
@@ -96,14 +96,14 @@
       </div>
       <div class="row">
         <div class="col-4">
-          <!-- srg_version -->
-          <strong>SRG Version</strong>
+          <!-- srg_version aka ID -->
+          <strong>SRG ID</strong>
           <i
-            v-if="tooltips['srg_version']"
+            v-if="tooltips['srg_id']"
             v-b-tooltip.hover.html
             class="mdi mdi-information"
             aria-hidden="true"
-            :title="tooltips['srg_version']"
+            :title="tooltips['srg_id']"
           />
         </div>
         <div class="col-8">{{ srg_rule.version }}</div>
@@ -133,6 +133,18 @@ export default {
       tooltips: {
         nist_control:
           "The NIST SP 800-53 Revision 4 Control Family that maps to the Common Control Indicator (CCI)",
+        cci:
+          "The Control Correlation Identifier (CCI) enables DoD organizations to trace STIG compliance to Information Assurance controls specified by the National Institutes of Standards and Technology (NIST) and mandated for Federal government agencies.",
+        srg_requirement:
+          "This is a sentence stating the requirement and is pre-populated from the Technology SRG.",
+        srg_vuln_discussion:
+          "The vulnerability discussion describes the risk of not complying with the requirement that is pre-populated from the Technology SRG.",
+        srg_check_text:
+          "The SRG Check Content provides a broad method of review and inspection. The STIG developer can use this information to determine what areas to inspect in the STIG.",
+        srg_fix_text:
+          "The SRG Fix Text provides a broad method of how to correct a system. The STIG developer can use this information to determine what areas to address in the STIG.",
+        srg_id:
+          "This is the ID for the SRG requirement. It may also contain identification information for a parent SRG document. May not be unique for a given STIG",
       },
       showSrgInformation: false,
     };
