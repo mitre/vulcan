9023336b45f3a8a9c07dafe31c75c71f9189d3ed
Ryan <rlakey81@outlook.com>
2023-02-01 14:58:25 -0700

    Update app/helpers/export_helper.rb
    Co-authored-by: Vanessa Fotso <46642178+vanessuniq@users.noreply.github.com>

--- app/helpers/export_helper.rb
+++ app/helpers/export_helper.rb
@@ -68,7 +68,7 @@ module ExportHelper # rubocop:todo Metrics/ModuleLength
         # so it needs to be manually kept track of
         csv_attributes = rule.csv_attributes
         if is_disa_export
-          check_text, fix_text = get_check_and_fix_text(rule.status)
+          check_text, fix_text = get_check_and_fix_text(rule.status).values_at('check_text', 'fix_text')
           if rule.status != 'Applicable - Configurable'
             csv_attributes[CSV_ATTRIBUTE_MAP[:export_checktext]] = check_text
             csv_attributes[CSV_ATTRIBUTE_MAP[:export_fixtext]] = fix_text
