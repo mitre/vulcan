12c9a32c98db96984fc2ee45f5a5f0fca0fcb46d
freddyfeelgood <darrickw@vmware.com>
2023-02-01 18:28:53 -0700

    Client side basic validation of url

--- app/javascript/components/rules/forms/AdditionalAnswerForm.vue
+++ app/javascript/components/rules/forms/AdditionalAnswerForm.vue
@@ -21,11 +21,15 @@
       @input="addOrUpdateAnswer($event, question.id)"
     />
     <template v-else-if="question.question_type === 'url'">
+      <template v-if="!validurl">
+        <span class="text-danger clickable float-right mr-3"> Must Start with HTTP or HTTPS! </span>
+      </template>
       <b-input
         v-if="!disabled"
         :id="`ruleEditor-additional-field-${question.id}`"
         :value="findAnswerText(question.id)"
         :class="inputClass(question.name)"
+        placeholder="Enter URL - make sure it starts with http or https..."
         @input="addOrUpdateAnswer($event, question.id)"
       />
     </template>
@@ -63,8 +67,19 @@ export default {
       default: false,
     },
   },
+  data: function () {
+    return {
+      validurl: true,
+    };
+  },
   methods: {
     addOrUpdateAnswer: function (event, question_id) {
+      if (this.question.question_type === "url" && event.length > 3) {
+        this.validurl = event.substring(0, 4).toLowerCase() == "http";
+      } else {
+        this.validurl = true;
+      }
+
       let all_answers = this.rule.additional_answers_attributes;
       let index = all_answers.findIndex((answer) => answer.additional_question_id === question_id);
       if (index !== -1) {
