0cfe51657ddc458a08f059d2a5035373491f0d02
Vanessa Fotso <vfotso@mitre.org>
2023-04-17 12:25:32 -0400

    Refactor to not include inspec body in DISA export
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- app/constants/export_constants.rb
+++ app/constants/export_constants.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 module ExportConstants
-  DISA_EXPORT_HEADERS = ["IA Control", "CCI", "SRGID", "STIGID", "Severity", "SRG Requirement", "Requirement",
-                         "SRG VulDiscussion", "VulDiscussion", "Status", "SRG Check", "Check", "SRG Fix",
-                         "Fix", "Status Justification", "Mitigation", "Artifact Description", "Vendor Comments", "InSpec Control Body"].freeze
+  DISA_EXPORT_HEADERS = ['IA Control', 'CCI', 'SRGID', 'STIGID', 'Severity', 'SRG Requirement', 'Requirement',
+                         'SRG VulDiscussion', 'VulDiscussion', 'Status', 'SRG Check', 'Check', 'SRG Fix',
+                         'Fix', 'Status Justification', 'Mitigation', 'Artifact Description', 'Vendor Comments'].freeze
+  OPTIONAL_EXPORT_HEADERS = ['InSpec Control Body'].freeze
+
+  EXPORT_HEADERS = [DISA_EXPORT_HEADERS, OPTIONAL_EXPORT_HEADERS].flatten.freeze
 end
--- app/constants/import_constants.rb
+++ app/constants/import_constants.rb
@@ -2,22 +2,22 @@
 
 module ImportConstants
   REQUIRED_MAPPING_CONSTANTS = {
-    srg_id: "SRGID",
-    stig_id: "STIGID",
-    rule_severity: "Severity",
-    title: "Requirement",
-    vuln_discussion: "VulDiscussion",
-    status: "Status",
-    check_content: "Check",
-    fixtext: "Fix",
-    status_justification: "Status Justification",
-    artifact_description: "Artifact Description",
+    srg_id: 'SRGID',
+    stig_id: 'STIGID',
+    rule_severity: 'Severity',
+    title: 'Requirement',
+    vuln_discussion: 'VulDiscussion',
+    status: 'Status',
+    check_content: 'Check',
+    fixtext: 'Fix',
+    status_justification: 'Status Justification',
+    artifact_description: 'Artifact Description'
   }.freeze
 
   OPTIONAL_MAPPING_CONSTANTS = {
-    vendor_comments: "Vendor Comments",
-    mitigation: "Mitigation",
-    inspec_control_body: "InSpec Control Body",
+    vendor_comments: 'Vendor Comments',
+    mitigation: 'Mitigation',
+    inspec_control_body: 'InSpec Control Body'
   }.freeze
 
   IMPORT_MAPPING = REQUIRED_MAPPING_CONSTANTS.merge(OPTIONAL_MAPPING_CONSTANTS)
