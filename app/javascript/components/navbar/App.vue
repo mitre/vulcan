a6ef8684ca66fb9a65ef62bc079e6e0ce5741db9
Vanessa Fotso <vfotso@mitre.org>
2023-05-02 12:04:32 -0400

    Update to read app version from package.json and display an alert if new version available
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- app/javascript/components/navbar/App.vue
+++ app/javascript/components/navbar/App.vue
@@ -3,9 +3,8 @@
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand id="heading" href="/">
         <i class="mdi mdi-radar" aria-hidden="true" />
-        VULCAN <span class="latest-release">{{ latestRelease }}</span>
+        VULCAN <span class="latest-release">{{ currentVersion }}</span>
       </b-navbar-brand>
-
       <b-navbar-toggle target="nav-collapse" />
 
       <b-collapse id="nav-collapse" is-nav>
@@ -34,12 +33,22 @@
         </div>
       </b-collapse>
     </b-navbar>
+    <b-alert
+      dismissible
+      fade
+      :show="updateAvailable"
+      class="text-center"
+      @dismissed="updateAvailable = false"
+    >
+      New version: Vulcan {{ latestRelease }} is now available!!
+    </b-alert>
   </div>
 </template>
 
 <script>
 import NavbarItem from "./NavbarItem.vue";
 import SrgIdSearch from "./SrgIdSearch.vue";
+import { version } from "../../../../package.json";
 
 export default {
   name: "Navbar",
@@ -69,6 +78,8 @@ export default {
   data() {
     return {
       latestRelease: "",
+      currentVersion: version,
+      updateAvailable: false,
     };
   },
   mounted() {
@@ -78,17 +89,19 @@ export default {
     fetchLatestRelease() {
       const owner = "mitre";
       const repo = "vulcan";
-
       // Make the API request to fetch the latest release
       fetch(`https://api.github.com/repos/${owner}/${repo}/releases/latest`)
         .then((response) => response.json())
         .then((data) => {
-          this.latestRelease = data.tag_name;
+          this.latestRelease = data.tag_name.substring(1);
+          this.updateAvailable = this.checkUpdateAvailable();
         })
         .catch((error) => {
-          // console.error("Error fetching latest release:", error);
           this.latestRelease = "";
         });
+    },
+    checkUpdateAvailable() {
+      return this.latestRelease.trim() !== "" && this.latestRelease !== this.currentVersion;
     },
   },
 };
