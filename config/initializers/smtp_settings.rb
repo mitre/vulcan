ac486ee6f39d30030113c64105aec31432da81a3
Sai Pavan Marlakunta <34048837+smarlaku820@users.noreply.github.com>
2023-05-30 04:59:28 +0530

    Enabling SMTP feature to send emails via ActionMailer (#584)
    * Enabling SMTP feature to send emails via ActionMailer
    
    * fixing rubocop offenses - TrailingEmptyLines
    
    * Linting Issues on .Vue files
    
    * SMTP controller logic modified to welcome project,component users; Created Instance Variables to Avoid DB Model access from views
    
    * Removed project reference as this is related to a component
    
    * reduced db round trips
    
    ---------
    
    Co-authored-by: Sai Pavan Marlakunta <smarlakunta@vmware.com>

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -6,6 +6,8 @@
 
 if Rails.env.production? && Settings.smtp.enabled
   Rails.application.config.action_mailer.delivery_method = :smtp
+  Rails.application.config.action_mailer.perform_deliveries = true
+  Rails.application.config.action_mailer.raise_delivery_errors = true
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = Settings.smtp.settings.transform_keys(&:to_sym)
