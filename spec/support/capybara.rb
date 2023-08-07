d7c70ce80040ec8e47dff94a30e8f46051d27d56
Vanessa Fotso <vfotso@mitre.org>
2023-08-07 08:01:09 -0400

    Temp fix for new chrome update
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Capybara.register_driver :chrome do |app|
+  # TODO: setting the required version is a temp fix for the new chrome update.
+  Webdrivers::Chromedriver.required_version = '114.0.5735.90'
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless') if ENV['CI']
 
