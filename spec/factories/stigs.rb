6744f88739222c58517b819d208d26074366f922
Vanessa Fotso <46642178+vanessuniq@users.noreply.github.com>
2023-08-24 14:04:08 -0400

    Fix: Capture STIG Name on Upload (#603)
    Made name field required for Stig, Refactor Stig and SecurityRequirementsGuide to capture name on initialize and updated spec
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- spec/factories/stigs.rb
+++ spec/factories/stigs.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
-XML_FILE = File.read('./spec/fixtures/files/U_A10_Networks_ADC_ALG_STIG_V2R1_Manual-xccdf.xml')
+XML_FILE_STIG = File.read('./spec/fixtures/files/U_A10_Networks_ADC_ALG_STIG_V2R1_Manual-xccdf.xml')
 
 FactoryBot.define do
   factory :stig do
     stig_id { FFaker::Name.name.underscore }
+    name { FFaker::Name.name }
     title { FFaker::Name.name }
     description { 'MyText' }
     version { "V#{rand(0..9)}R#{rand(0..9)}" }
-    xml { XML_FILE }
+    xml { XML_FILE_STIG }
     benchmark_date { '2023-07-20' }
   end
 end
