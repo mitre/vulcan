0e40860e97c536e08a9a9d39b1bfb6b0fdd0683e
Vanessa Fotso <vfotso@mitre.org>
2023-11-09 11:41:47 -0500

    updated deprecated capybara [:capabilities] field to :options
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,7 +7,7 @@ Capybara.register_driver :chrome do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.read_timeout = 120
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client, capabilities: [options])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client, options: options)
 end
 
 Capybara.default_driver = :chrome
