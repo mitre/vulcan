d496578c8234ed6134ed3bb91f1d11405b2036f6
Robert Clark <rbclarkdev@gmail.com>
2025-01-26 01:57:17 -0500

    Upgrade from Rails 6.1 to 7.0

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,13 +1,3 @@
 # frozen_string_literal: true
 
-Capybara.register_driver :chrome do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless') if ENV['CI']
-
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.read_timeout = 120
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client, capabilities: [options])
-end
-
-Capybara.default_driver = :chrome
+Capybara.default_driver = :selenium_chrome_headless
