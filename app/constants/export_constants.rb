5d4d784168e6ab534887be6b468bedc2a010057c
skykam87 <skykam87@yahoo.com>
2025-03-04 20:03:47 +0000

    Flipped Artifact Description and Status Justification columns to correctly match DISA template.

--- app/constants/export_constants.rb
+++ app/constants/export_constants.rb
@@ -3,7 +3,7 @@
 module ExportConstants
   DISA_EXPORT_HEADERS = ['IA Control', 'CCI', 'SRGID', 'STIGID', 'SRG Requirement', 'Requirement', 'SRG VulDiscussion',
                          'VulDiscussion', 'Status', 'SRG Check', 'Check', 'SRG Fix', 'Fix', 'Severity', 'Mitigation',
-                         'Status Justification', 'Artifact Description', 'Vendor Comments'].freeze
+                         'Artifact Description', 'Status Justification', 'Vendor Comments'].freeze
   OPTIONAL_EXPORT_HEADERS = ['InSpec Control Body'].freeze
 
   EXPORT_HEADERS = [DISA_EXPORT_HEADERS, OPTIONAL_EXPORT_HEADERS].flatten.freeze
