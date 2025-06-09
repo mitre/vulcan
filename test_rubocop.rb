108d7a268c22900145693afc56a1f1a49a46ea12
Aaron Lippold <lippold@gmail.com>
2025-06-09 10:42:56 -0400

    Remove test file

--- test_rubocop.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Test file with Rubocop violations
-class TestRubocop
-  def bad_method
-    puts 'This has trailing whitespace'
-    1 + 2 # No spaces around operators
-  end
-end
