154ed22ecd14e69b8786818f719317acad56f69a
Aaron Lippold <lippold@gmail.com>
2025-08-16 12:48:27 -0400

    fix: Update Heroku Review App deployment configuration
    - Simplify Review App postdeploy script
    - Add environment check to create_admin.rb script
    - Review Apps now use schema-only setup
    
    Authored by: Aaron Lippold<lippold@gmail.com>

--- create_admin.rb
+++ create_admin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This script is for development and testing use only
-unless Rails.env.development? || Rails.env.test?
+unless Rails.env.local?
   raise 'ERROR: This script is only for use in development or test environments. ' \
         'Creating admin@example.com in production is a security risk!'
 end
