26fb426c6e0a4d0c25bda0f973d94fd4ad47bef9
Aaron Lippold <lippold@gmail.com>
2026-02-08 19:34:20 -0500

    chore: disable RSpec/MessageChain cop and fix NonNilCheck style
    Authored by: Aaron Lippold<lippold@gmail.com>

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -17,7 +17,7 @@ class ComponentsController < ApplicationController
   before_action :authorize_admin_component, only: %i[update], if: lambda {
     # Check if advanced_fields param exists (not if it's truthy)
     # Both true and false are attempts to modify an admin-only field
-    params.dig(:component, :advanced_fields) != nil
+    !params.dig(:component, :advanced_fields).nil?
   }
 
   before_action :authorize_viewer_component, only: %i[show], if: -> { @component.released == false }
